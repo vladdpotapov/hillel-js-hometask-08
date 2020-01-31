53fc656cf840f09507b4021c0ccc83e465b46830
PerSick <vlad461@ukr.net>
2020-01-31 14:09:58 +0300

    Task #02 - done

--- Task_02/js/main.js
+++ Task_02/js/main.js
@@ -5,25 +5,23 @@
 // при клике на вторую - переадресовывается на другой сайт (по ранее введенной ссылке). 
 // Реализовать проверку на http / https. Если протокол не указан - добавляем.
 
-let button01 = document.getElementById('btn1');
+const button01 = document.getElementById('btn1');
+const button02 = document.getElementById('btn2');
+let userUrl;
+
 button01.addEventListener('click', function(){
-    let userUrl = prompt('Enter URL');
+    userUrl = prompt('Enter URL');
     if (userUrl.indexOf('https://') +1) {
         return userUrl;
-        // console.log(userUrl);
     } else {
         let string = userUrl.split();
         string.unshift('https://');
         userUrl = string.join('');
         return userUrl;
-        // console.log(userUrl);
-        // let userAddress = string.join('');
-        // return userAddress;
     }
 });
 
-// let button02 = document.getElementById('btn2');
-// button02.addEventListener('click', function() {
-//     location.href = userAdress;
-// });
+button02.addEventListener('click', function() {
+    location.href = userUrl;
+});
 
