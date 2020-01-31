12337701d47a28277f6be9115d2f51ad52028d33
PerSick <vlad461@ukr.net>
2020-01-31 13:53:12 +0300

    Task# 01 - Task conditions added

--- Task_01/js/main.js
+++ Task_01/js/main.js
@@ -1,3 +1,8 @@
+// TASK #01:
+
+// Имеется текстовое поле на странице. 
+// При фокусировке на этом поле сбоку появляется <div> с информацией. 
+// При пропаже фокуса - <div> так же пропадает.
 
 const input = document.getElementById('input');
 const wrap  = document.getElementById('input-wrap');
