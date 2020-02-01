70c395a36a0942b2d9a6df0ccb98574a30da296e
PerSick <vlad461@ukr.net>
2020-02-01 23:02:53 +0300

    task #05 - done

--- Task_05/js/main.js
+++ Task_05/js/main.js
@@ -1,53 +1,8 @@
-const button = document.getElementById('button');
-const frame  = document.getElementById('content-frame');
-let voteBox;
-let voteButton;
-let voteCounterBox;
-let voteCounter;
+// Get HTML Collection of buttons and divs:
+const buttons = document.getElementsByClassName('button');
+const counterBoxes = document.getElementsByClassName('poll__counter-box');
 
-button.addEventListener('click', function() {;
-    createVoteInterface();
-});
-
-function createVoteBox() {
-    let box = document.createElement('div');
-    box.setAttribute('class', 'vote-box');
-    box.setAttribute('id', 'vote-box');
-    frame.appendChild(box);
-    return box;
+// Function takes IDs of each element of collections:
+for(let i = 0; i < buttons.length; i++) {
+    createCounter(buttons[i].id, counterBoxes[i].id);
 }
-
-function createVoteButtons() {
-    for (let i = 1; i <= 5; i++) {
-        let btn = document.createElement('button');
-        btn.setAttribute('id', 'vote-button' + i);
-        btn.setAttribute('class', 'vote-button');
-        voteBox.appendChild(btn);
-    }
-    return voteBox;
-}
-
-function createCounterBox() {
-    let counter = document.createElement('div');
-    counter.setAttribute('class', 'counter-box');
-    frame.appendChild(counter);  
-    return counter;
-}
-
-function createCounters() {
-    for (let i = 1; i <= 5; i++) {
-        let count = document.createElement('div');
-        count.setAttribute('class', 'counter');
-        count.innerHTML = 'digit';
-        voteCounterBox.appendChild(count);
-    }
-    return count;
-}
-
-function createVoteInterface() {
-    voteBox = createVoteBox();
-    voteButton = createVoteButtons();
-    voteCounterBox = createCounterBox();
-    voteCounter = createCounters();
-}
-
--- Task_05a/js/main.js
+++
@@ -1,8 +0,0 @@
-// Get HTML Collection of buttons and divs:
-const buttons = document.getElementsByClassName('button');
-const counterBoxes = document.getElementsByClassName('poll__counter-box');
-
-// Function takes IDs of each element of collections:
-for(let i = 0; i < buttons.length; i++) {
-    createCounter(buttons[i].id, counterBoxes[i].id);
-}
